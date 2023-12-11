231235d8bff1b76ff00c2cda23cacbb5729e1716
andrea <andreamaccarinelli18@gmail.com>
2023-12-11 18:18:52 +0100

    Intersezione ottenuta e codice ripulito !!

--- confronto.py
+++ confronto.py
@@ -12,57 +12,48 @@ from libreriaUNO import save_array_to_txt
 
 def main():
 
+    #Inizializzo i percorsi ai file che utilizzerò
     PATH="/Users/andreamaccarinelli/Desktop/SDSS/"
     All=PATH+"/gal_info_dr7_v5_2.fits"
-    #Bcg=PATH+"/DATA_BCG.txt" #versione con C4 in formato txt
     Bcg = PATH+"sampleC4.fits"
 
+
+    #Raccolgo RA e DEC dal Catalogo SDSS/DR7
     obj=fits.open(All)
     data=obj[1].data
-    # RA E DEC ALL SAMPLE
     ra,dec=data["RA"],data["DEC"]
 
-    # RA E DEC IN BCG
-    #rab,decb= np.loadtxt(Bcg, usecols=[1,2], unpack=True,dtype=float)
+    # Raccolgo RA E DEC dal Catalogo C4
     obj2=fits.open(Bcg)
     data2 = obj2[1].data
     rab,decb=data2["RAdeg"],data2["DEdeg"]
 
-    #ESEMPIO UN CASO i=0
+    #Inizializzo le variabili di zoom
     k1,k2=0.01,0.001
     incr = 0.0015
-    i=0
-    #TROVA INDICE (ival) CORRISPONDENTE A BCG IN FILE TOTALE SAMPLE
-    ival = np.where((ra >= rab[i]-k2) & (ra <= rab[i]+k2) & (dec >= decb[i]-k2) & (dec <= decb[i]+k2))[0][0]
-    #ival è un array bidimensionale con nella prima colonna l'elenco delle posizioni in cui la condizione dentro le parentesi tonde
-    #della funzione np.where si verificano e nella seconda dove non si verificano.
-    #così facendo la parte che compare sopra con le due parentesi quadre seleziona il primo elemento della tabella che si crea, 
-    #nella fattispecie il primo elemento in cui la condizione si avvera 
 
-    #CONFRONTA VALORI
-    print(ra[ival],dec[ival]," == ",rab[i],decb[i])
-    print("L'indice cui corrisponde il primo oggetto di C4, entro un intorno di 3.6 arcosecondi è ",ival)
-
-    #inizializzo un vettore di zeri di dimensione pari a quella del catalogo C4 in cui cambiare i valori poi successivamente 
-    indici_ottenuti = np.zeros(len(rab))
+    
+    #inizializzo una lista vuota per contenere gli indici in comune 
+    indici_ottenuti = []
 
     #cerco dunque di implementare il tutto per l'intero catalogo C4 memorizzando gli indici in un array per il momento
     for i in range(len(rab)):
-        ival = np.where((ra >= rab[i]-k1) & (ra <= rab[i]+k1) & (dec >= decb[i]-k1) & (dec <= decb[i]+k1))
+        ival = np.where((ra >= rab[i]-k1) & (ra <= rab[i]+k1) & (dec >= decb[i]-k1) & (dec <= decb[i]+k1))[0]
         if(len(ival)==0 ):
          k2=k2+incr
+         continue
         elif (len(ival)>1):
             k2=k2-incr
-        #indici_ottenuti = ival
+            continue
+        elif (len(ival)==1):
+           indici_ottenuti.append(ival)
+        
+    indici_ottenuti = np.array(indici_ottenuti)
+    
+    #Conclusione delle operazioni
+    print('La dimensione della intersezione è :', len(indici_ottenuti))
+    save_array_to_txt(indici_ottenuti,'ElementiComuniC4.txt' )
 
-
-
-      #RESTA DA IMPLEMENTARE IL SISTEMA DI SALVATAGGIO DEGLI INDICI IN UN FILE .TXT
-    #save_array_to_txt(indici_ottenuti,'ElementiComuniC4.txt' )
-
-    #print(len(ival))
-    #indici_ottenuti = ival[:,0]
-    #save_array_to_txt(indici_ottenuti,'ElementiComuniC4.txt' )
 
 
       
